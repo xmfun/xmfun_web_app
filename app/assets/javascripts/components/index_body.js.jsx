46b7bde8368fd95ca6fa0f0316840d1446c80f69
lizhang <ycy828f@gmail.com>
2015-09-30 19:59:52 -0700

    lz - get search works

--- app/assets/javascripts/components/index_body.js.jsx
+++ app/assets/javascripts/components/index_body.js.jsx
@@ -1,19 +1,35 @@
 var IndexBody = React.createClass({
   propTypes: {
     searchUrl: React.PropTypes.string,
-    authToken: React.PropTypes.string
+    authToken: React.PropTypes.string,
+    searchInput: React.PropTypes.string
   },
   getDefaultProps: function() {
     return {
       authToken: '',
-      searchUrl: ''
+      searchUrl: '',
+      searchInput: ''
     };
+  },
+  componentDidMount: function() {
+    this.refs.MainHeader.getDOMNode().style.textAlign = "center";
+    this.refs.MainHeader.getDOMNode().style.marginTop = "150px";
+    this.refs.MainHeader.getDOMNode().style.marginBottom = "30px";
   },
   render: function () {
     return (
       <div>
-        <h1>Home#index</h1>
-        <SearchBar searchUrl={this.props.searchUrl} authToken={this.props.authToken}/>
+        <div className="container container-table">
+          <div className="row vertical-center-row">
+            <div className="col-md-4 col-md-offset-4">
+              <h1 ref="MainHeader">XMFun</h1>
+              <SearchBar ref="SearchBar"
+                        searchUrl={this.props.searchUrl}
+                        authToken={this.props.authToken}
+                        searchInput={this.props.searchInput}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -22,12 +38,14 @@ var IndexBody = React.createClass({
 var SearchBar = React.createClass({
   propTypes: {
     searchUrl: React.PropTypes.string,
-    authToken: React.PropTypes.string
+    authToken: React.PropTypes.string,
+    searchInput: React.PropTypes.string
   },
   getDefaultProps: function() {
     return {
       authToken: '',
-      searchUrl: ''
+      searchUrl: '',
+      searchInput: ''
     };
   },
   getInitialState: function () {
@@ -40,26 +58,25 @@ var SearchBar = React.createClass({
     console.log(this.state.searchInput);
     console.log(this.props.authToken);
   },
+  componentDidMount: function() {
+    if (this.props.searchInput.length > 0) {
+      this.refs.srchInput.getDOMNode().value = this.props.searchInput;
+    }
+  },
   render: function () {
     return (
-      <div className="container container-table">
-        <div className="row vertical-center-row">
-          <div className="col-md-4 col-md-offset-4">
-            <form className="form-search" action={this.props.searchUrl} method='post' onSubmit={this.handleSubmit}>
-              <input type="hidden" value={this.props.authToken} name="authenticity_token"/>
-              <div className="input-group add-on">
-                <input type="search" className="form-control" placeholder="Search"
-                      name="srch_term" onChange={this.handleSearchInput}/>
-                <div className="input-group-btn">
-                  <button className="btn btn-default" type="submit">
-                    <i className="glyphicon glyphicon-search"></i>
-                  </button>
-                </div>
-              </div>
-            </form>
+      <form className="form-search" action={this.props.searchUrl} method='post' onSubmit={this.handleSubmit}>
+        <input type="hidden" value={this.props.authToken} name="authenticity_token"/>
+        <div className="input-group add-on">
+          <input ref="srchInput" type="search" className="form-control" placeholder="Search for Xiami music"
+                name="srch_term" onChange={this.handleSearchInput}/>
+          <div className="input-group-btn">
+            <button className="btn btn-default" type="submit">
+              <i className="glyphicon glyphicon-search"></i>
+            </button>
           </div>
         </div>
-      </div>
+      </form>
     );
   }
 });
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,7 @@ class HomeController < ApplicationController
 
   def search
     p "PARAM IS #{params}"
+    @search_input = params[:srch_term]
+    render :index
   end
 end
